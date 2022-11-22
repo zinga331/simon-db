f0bae3edbff3ba7801e9a0b8da981f661cef926b
Lee <lee.jensen@agilix.com>
2022-11-21 22:30:02 -0700

    fix(deploy) to use subdomains

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const DB = require("./database.js");
+const DB = require('./database.js');
 
-// The service name and port. We use these to partition it from other running services when running in the production environment.
-const serviceName = "simon-db";
-const port = 3001;
+// The service port. In production the application is statically hosted by the service on the same port.
+const port = process.env.SIMONDBPORT || 3000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
 // Server up the applications static content
-app.use(`/${serviceName}`, express.static("application"));
+app.use(express.static('application'));
 
 // Router for service endpoints
 var apiRouter = express.Router();
-app.use(`/${serviceName}/api`, apiRouter);
+app.use(`/api`, apiRouter);
 
 // GetScores
-apiRouter.get("/scores", async (_req, res) => {
+apiRouter.get('/scores', async (_req, res) => {
   const scores = await DB.getHighScores();
   res.send(scores);
 });
 
 // SubmitScore
-apiRouter.post("/score", async (req, res) => {
+apiRouter.post('/score', async (req, res) => {
   DB.addScore(req.body);
   const scores = await DB.getHighScores();
   res.send(scores);
@@ -31,7 +30,7 @@ apiRouter.post("/score", async (req, res) => {
 
 // Redirect back to the home page if the path is unknown
 app.use((_req, res) => {
-  res.redirect(`/${serviceName}`);
+  res.redirect(`/`);
 });
 
 app.listen(port, () => {
