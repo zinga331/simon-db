c360ee2444dd3dc61c422b4380a6a3341cfcab4a
Lee <lee.jensen@agilix.com>
2023-04-13 16:49:17 -0600

    fix(auth) no env vars

--- database.js
+++ database.js
@@ -1,17 +1,19 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
 
-const userName = process.env.MONGOUSER;
-const password = process.env.MONGOPASSWORD;
-const hostname = process.env.MONGOHOSTNAME;
-
-if (!userName) {
-  throw Error('Database not configured. Set environment variables');
-}
-
-const url = `mongodb+srv://${userName}:${password}@${hostname}`;
-
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const scoreCollection = client.db('simon').collection('score');
+const db = client.db('simon');
+const scoreCollection = db.collection('score');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
 
 async function addScore(score) {
   const result = await scoreCollection.insertOne(score);
@@ -19,13 +21,13 @@ async function addScore(score) {
 }
 
 function getHighScores() {
-  const query = {score: {$gt: 0}};
+  const query = { score: { $gt: 0 } };
   const options = {
-    sort: {score: -1},
+    sort: { score: -1 },
     limit: 10,
   };
   const cursor = scoreCollection.find(query, options);
   return cursor.toArray();
 }
 
-module.exports = {addScore, getHighScores};
+module.exports = { addScore, getHighScores };
