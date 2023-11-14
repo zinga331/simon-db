e2394a0aea1fed7fc23a4c0cefa7b857525bd2d7
Christian Hall <christianjameshall@gmail.com>
2023-11-14 13:01:53 -0700

    Included database.js to simon db repo.

--- database.js
+++ database.js
@@ -1,17 +1,19 @@
-const { MongoClient } = require('mongodb');
-const config = require('./dbConfig.json');
+const { MongoClient } = require("mongodb");
+const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
-const scoreCollection = db.collection('score');
+const db = client.db("simon");
+const scoreCollection = db.collection("score");
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
   await client.connect();
   await db.command({ ping: 1 });
 })().catch((ex) => {
-  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  console.log(
+    `Unable to connect to database with ${url} because ${ex.message}`
+  );
   process.exit(1);
 });
 
